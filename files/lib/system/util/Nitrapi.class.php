105ef558389fd939166b612501d950420b7c5d5b
Titus Kirch <requests@tkirch.dev>
2021-01-05 19:45:44 +0100

    feat: rc

---
+++ files/lib/system/util/Nitrapi.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace wcf\util;
+
+require_once WCF_DIR . 'lib/system/api/nitrapi-php/autoload.php';
+
+class Nitrapi extends \Nitrapi\Nitrapi
+{
+    public function __construct($options = array(), $url = NITRAPI_LIVE_URL)
+    {
+        parent::__construct(NITRAPI_ACCESS_TOKEN, $options, $url);
+    }
+}
